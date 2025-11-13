7d99a5255ec1e8ef53a65e440bfa372eff9f1c83
Menga wanji <martialwanji@gmail.com>
2025-11-13 17:14:26 +0100

    Include a proper detailed README.md file

--- src/config/database.js
+++ src/config/database.js
@@ -10,9 +10,8 @@ const  pool = new Pool({
   database: process.env.DB_NAME || 'todo_db',
   password: process.env.DB_PASSWORD || 'newpass',
   port: parseInt(process.env.DB_PORT) || 5432,
+  ssl: { rejectUnauthorized: false}
 })
-
-
 
 
 // Test connection
